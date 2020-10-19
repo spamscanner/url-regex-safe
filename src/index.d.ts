35584dc67a6d1373b4ffe8bd05b29940e92e7625
Stuart Long <stuartlo@microsoft.com>
2020-10-19 10:56:53 -0700

    fix: add namespace declaration

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,14 +1,16 @@
-export type Options = Partial<{
-    exact: boolean;
-    strict: boolean;
-    auth: boolean;
-    localhost: boolean;
-    parens: boolean;
-    apostrophes: boolean;
-    trailingPeriod: boolean;
-    ipv4: boolean;
-    ipv6: boolean;
-    tlds: string[];
-    returnString: boolean;
-}>;
-export default function createUrlRegExp (options?: Options): RegExp;
+declare module "url-regex-safe" {
+    export type Options = Partial<{
+        exact: boolean;
+        strict: boolean;
+        auth: boolean;
+        localhost: boolean;
+        parens: boolean;
+        apostrophes: boolean;
+        trailingPeriod: boolean;
+        ipv4: boolean;
+        ipv6: boolean;
+        tlds: string[];
+        returnString: boolean;
+    }>;
+    export default function createUrlRegExp (options?: Options): RegExp;
+}
